298f8626020a1607b224519b4d6f78ed1346e3c1
muriz-dev <mhmdrizki.dev@gmail.com>
2026-07-16 14:13:03 +0700

    feat(auth): register auth pages to app router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -2,10 +2,17 @@ import 'package:auto_route/auto_route.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/register_page.dart';
+
 part 'app_router.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
-  List<AutoRoute> get routes => [RedirectRoute(path: '/', redirectTo: '/home')];
+  List<AutoRoute> get routes => [
+        AutoRoute(page: LoginRoute.page, initial: true),
+        AutoRoute(page: RegisterRoute.page),
+        // RedirectRoute(path: '/', redirectTo: '/login')
+      ];
 }
